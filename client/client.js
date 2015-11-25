0c838bde546b0603c3eca7a2ad7fc97177369216
Marios Antonoudiou <marios@artime.gr>
2015-11-25 11:49:59 +0200

    Try to init fb lib better

--- client/client.js
+++ client/client.js
@@ -1,20 +1,23 @@
-Meteor.call('facebookAppId', function(error, appId) {
-  window.fbAsyncInit = function() {
+Meteor.startup(function() {
+  Session.setDefault('facebookInitialized', false);
+  Meteor.call('facebookAppId', function(error, appId) {
     FB.init({
-      appId      : appId,
-      status     : true,
-      xfbml      : true,
-      version    : 'v2.4'
-    });
-  };
-});
+        appId      : appId,
+        status     : true,
+        xfbml      : true,
+        version    : 'v2.5'
+      });
+      Session.set('facebookInitialized', true);
+      FB.XFBML.parse();
 
+  });
+});
 Template.registerHelper('fbLikeConfig', function() {
   return {href: 'http://anakiklotetrise.herrco.gr', colorscheme: 'dark'}
 });
 
 Template.socialBar.rendered = function() {
-  try {
+  if (Session.get('facebookInitialized')) {
     FB.XFBML.parse();
-  } catch(e) {}
+  }
 };
